e98b6635cc3f7582f3f2dbb6deae4ba8d3b68350
Richard Mora <llody777@gmail.com>
2026-04-17 21:18:29 -0700

    Complete operate function and added buttons to index.html with values

--- script.js
+++ script.js
@@ -11,9 +11,23 @@ const multiply = function(num1, num2) {
 }
 
 const divide = function(num1, num2) {
+    if (num2 === 0) {
+        alert("Division by zero is not allowed");
+    }
     return num1 / num2;
 }
 
 const operate = function(operator, num1, num2) {
-    operator(num1, num2);
+    switch (operator) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+        default:
+            break;
+    }
 }
