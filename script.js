bdbeedcf9537ef787cf4d423a1c9007c5b348cab
Richard Mora <llody777@gmail.com>
2026-05-06 20:20:30 -0700

    redid eventlistener where it stores two variables using the same buttons to properly display more than one digit

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 const display = document.querySelector("#display");
 let btns = document.querySelectorAll(".btn");
+let equal = document.querySelector("#equals");
 
-let num1, num2, operator;
+let num1 = "";
+let num2 = ""; 
+let operator = "";
 
 display.textContent = "";
 
 btns.forEach((button) => {
-    button.addEventListener("click", (e) => {
+    button.addEventListener("click", () => {
         if (operator === "") {
-            num1 = Number.parseInt(e.target.value);
-            display.textContent += num1;
+            num1 += button.value;
+            display.textContent = Number.parseInt(num1);
             console.log("num1 is " + num1);
         } else {
-            num2 = Number.parseInt(e.target.value);
-            display.textContent += num2;
+            num2 += button.value;
+            display.textContent = Number.parseInt(num2);
             console.log("num2 is " + num2);
         }
-        console.log(e.target.value);
         
     });
 });
 
 const add = function(num1, num2) {
+    display.textContent = num1 + num2;
     return num1 + num2;
 }
 
 const subtract = function(num1, num2) {
+    display.textContent = num1 - num2;
     return num1 - num2;
 }
 
 const multiply = function(num1, num2) {
+    display.textContent = num1 * num2;
     return num1 * num2;
 }
 
@@ -37,6 +42,7 @@ const divide = function(num1, num2) {
     if (num2 === 0) {
         alert("Division by zero is not allowed");
     }
+    display.textContent = num1 / num2;
     return num1 / num2;
 }
 
@@ -59,14 +65,13 @@ let submits = document.querySelectorAll(".operator");
 
 submits.forEach((submit) => {
 
-    submit.addEventListener("click", (e) => {
-        operator = e.target.value;
+    submit.addEventListener("click", () => {
         display.textContent = "";
+        operator = submit.value;
         console.log(operator);
-        operate(operator, num1, num2);
+        num2 = "";
     });
 });
 
-let equal = document.querySelector("#equals");
 
 equal.addEventListener("click", operate(operator, num1, num2));
